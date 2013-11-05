2a6205a8f6a0f75464d3357b6693a303131de54f
ruanpienaar <ruan800@gmail.com>
2013-11-05 12:28:11 +0000

    added initial functions

--- src/ofs_handler.erl
+++ src/ofs_handler.erl
@@ -10,14 +10,31 @@
 
 -behaviour(gen_server).
 
--export([start_link/0]).
+-export([start_link/0, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-	 terminate/2, code_change/3]).
+-export([ handle_connect/2,
+	  handle_message/2,
+	  handle_disconnect/2
+	]).
 
 -define(SERVER, ?MODULE). 
 
 -record(state, {}).
+
+%%------------------------------------------------------------------
+-spec handle_connect(Switch :: term(), Version :: term()) -> ok | {error,Reason :: term()}.
+handle_connect(Switch, Version) ->
+    ok.
+
+-spec handle_message(Switch :: term(), Version :: term()) -> ok | {error,Reason :: term()}.
+handle_message(Switch, Msg) ->
+    ok.
+
+-spec handle_disconnect(Switch :: term(), Reason :: term()) -> ok | {error,Reason :: term()}.
+handle_disconnect(Switch, Reason) ->
+    ok.
+
+%%------------------------------------------------------------------
 
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
