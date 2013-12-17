9590dc5800e71db8d9b64ca2108aae6941ad477d
ruanpienaar <ruan800@gmail.com>
2013-12-17 11:33:07 +0000

    fixing dependancies for loom

--- start-dev.sh
+++ start-dev.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-exec erl -pa deps/*/ebin -pa ebin \
-    -boot start_sasl \
-    -config system.config \
-    -sname local_ofs_handler \
-    -init_debug
+#exec erl -pa $PWD/deps/*/ebin -pa $PWD/ebin \
+#    -boot start_sasl \
+#    -config system.config \
+#    -sname local_of_driver \
+#    -s of_driver_app 
+
+cd `dirname $0`
+exec erl -sname ofs_handler -config $PWD/sys.config -pa $PWD/ebin $PWD/deps/*/ebin $PWD/tests -boot start_sasl -mnesia dir "'"$PWD"/Mnesia'"
